39ccdc299877180a7fd4ef4aa691023358deb284
amide <amid.ehsan@gmail.com>
2025-05-16 16:17:19 +0800

    2025-05-16

--- src/integrations/supabase/types.ts
+++ src/integrations/supabase/types.ts
@@ -99,14 +99,14 @@ export type Database = {
         }
         Relationships: [
           {
-            foreignKeyName: "tbpractice_exampleid_fkey"
+            foreignKeyName: "tblpractice_exampleid_fkey"
             columns: ["exampleid"]
             isOneToOne: false
             referencedRelation: "tblexample"
             referencedColumns: ["exampleid"]
           },
           {
-            foreignKeyName: "tbpractice_userid_fkey"
+            foreignKeyName: "tblpractice_userid_fkey"
             columns: ["userid"]
             isOneToOne: false
             referencedRelation: "tbuser"
